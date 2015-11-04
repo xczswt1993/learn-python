08b4e1f54e76ee43f7d3bf7a1212d0fb500ce100
stone <stone931027@gmail.com>
2015-11-04 22:35:03 +0800

    修改了正则表达式

--- crawler_ex1.py
+++ crawler_ex1.py
@@ -3,12 +3,32 @@ __author__ = 'stone'
 
 import urllib
 import urllib2
-
+import  re
 url = 'http://www.heibanke.com/lesson/crawler_ex00/'
 
-req = urllib2.Request(url)
-response = urllib2.urlopen(req)
-page = response.read().decode('utf-8')
+#r'数字(.*?)</h3>'
+#
 
-print page
+def search_num(pattern,url):
+    req = urllib2.Request(url)
+    response = urllib2.urlopen(req)
+    page = response.read()
+    result = re.search(pattern,page)
+    num = result.group(1)
+    return num
+
+pattern1 = r'数字(.*?)</h3>'
+nextNum = search_num(pattern1,url)
+
+pattern2 = r'数字是(.*?)\.'
+while True:
+    nextUrl = url + str(nextNum)
+    nextNum = search_num(pattern2,nextUrl)
+    print nextNum
+
+
+
+
+
+
 
